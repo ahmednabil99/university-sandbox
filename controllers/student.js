37c98352edbd1a62e482bcfc7247798c7e56d40e
ahmednabil99 <61230368+ahmednabil99@users.noreply.github.com>
2021-06-17 05:31:47 +0200

    Update student.js

--- controllers/student.js
+++ controllers/student.js
@@ -28,7 +28,17 @@ module.exports = {
     },
 
     postRegistration: async (req, res) => {
-        // ..
+        const Ch_courses = req.body.courses;
+        for (var i = 0; i < Ch_courses; i++) {
+            const ChosenCourses = await sqlQuery('INSERT INTO registration (student_id, course_id, mark) VALUES (${studentID}, course , NULL)'  );
+        }
+        
+        console.log(req.body);
+        return res.redirect('student/allowedCourses', {
+            activePath: '/students',
+            registration: result
+        });
+
     },
 
     getAddMarks: async (req, res) => {
